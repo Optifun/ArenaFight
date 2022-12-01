78580fde97ade1e016d87671653738e25b81f706
Dmitry Frolov <frdima19@gmail.com>
2022-12-02 02:02:40 +0700

    Add ArenaInstaller to run ECS

---
+++ Assets/Scripts/Infrastructure/Installers/ArenaInstaller.cs
@@ -0,0 +1,36 @@
+﻿using Leopotam.Ecs;
+using UnityEngine;
+using Zenject;
+
+namespace Infrastructure.Installers
+{
+    public class ArenaInstaller : MonoInstaller, IInitializable, ITickable
+    {
+        private EcsWorld _world;
+        private EcsSystems _systems;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<IInitializable>().To<ArenaInstaller>().FromInstance(this);
+        }
+
+        public void Initialize()
+        {
+            _world = new EcsWorld();
+            _systems = new EcsSystems(_world);
+
+            _systems.Add(new TestSystem());
+
+            _systems.Init();
+        }
+
+        public void Tick() =>
+            _systems.Run();
+    }
+
+    public struct TestSystem : IEcsInitSystem
+    {
+        public void Init() =>
+            Debug.Log("ECS is working");
+    }
+}
