78d6ce34d6dee8e0df798047aa1a8ca946d78d5d
Dmitry Frolov <frdima19@gmail.com>
2022-12-02 01:07:04 +0700

    Create Level 1 config

--- Assets/Scripts/StaticData/ArenaInfo.cs
+++ Assets/Scripts/StaticData/ArenaInfo.cs
@@ -6,6 +6,6 @@ namespace StaticData
     [CreateAssetMenu(menuName = "Common/Arena information", fileName = "Level 0", order = 0)]
     public class ArenaInfo : ScriptableObject
     {
-        [field: SerializeField] public SceneAsset Scene { get; }
+        [field: SerializeField] public SceneAsset Scene { get; private set; }
     }
 }
