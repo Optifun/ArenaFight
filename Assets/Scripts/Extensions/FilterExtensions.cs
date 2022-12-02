deede03517271754adaeaabfa2993b0176293cb8
Dmitry Frolov <frdima19@gmail.com>
2022-12-02 18:56:09 +0700

    Add MoveCharacterSystem

---
+++ Assets/Scripts/Extensions/FilterExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Leopotam.Ecs;
+
+namespace Extensions
+{
+    public static class FilterExtensions
+    {
+        public static bool TryGet1<T1>(this EcsFilter<T1> filter, out EcsComponentRef<T1> component)
+            where T1 : struct
+        {
+            var enumerator = filter.GetEnumerator();
+            if (enumerator.MoveNext())
+            {
+                component = filter.Get1Ref(enumerator.Current);
+                return true;
+            }
+
+            component = default;
+            return false;
+        }
+
+        public static bool TryGet1<T1, T2>(this EcsFilter<T1, T2> filter, out EcsComponentRef<T1> component)
+            where T1 : struct where T2 : struct
+        {
+            var enumerator = filter.GetEnumerator();
+            if (enumerator.MoveNext())
+            {
+                component = filter.Get1Ref(enumerator.Current);
+                return true;
+            }
+
+            component = default;
+            return false;
+        }
+
+        public static bool TryGet2<T1, T2>(this EcsFilter<T1, T2> filter, out EcsComponentRef<T2> component)
+            where T1 : struct where T2 : struct
+        {
+            var enumerator = filter.GetEnumerator();
+            if (enumerator.MoveNext())
+            {
+                component = filter.Get2Ref(enumerator.Current);
+                return true;
+            }
+
+            component = default;
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Features/Character/Systems/MoveCharacterSystem.cs
@@ -0,0 +1,27 @@
+﻿using Extensions;
+using Game.Features.Movement;
+using Game.Features.Tags;
+using Leopotam.Ecs;
+using UnityEngine;
+
+namespace Game.Features.Character.Systems
+{
+    public class MoveCharacterSystem : IEcsRunSystem
+    {
+        private EcsFilter<InputComponent> _input;
+        private EcsFilter<CharacterTag, PositionComponent> _character;
+
+        public void Run()
+        {
+            foreach (var i in _character)
+            {
+                ref var playerPosition = ref _character.Get2(i);
+                if (_input.TryGet1(out var input) && !input.IsNull())
+                {
+                    ref var inputComponent = ref input.Unref();
+                    playerPosition.Position += inputComponent.Direction * Time.fixedTime;
+                }
+            }
+        }
+    }
+}
