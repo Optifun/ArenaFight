6613ed56c39828330062f0269f573dda351fff3e
Dmitry Frolov <frdima19@gmail.com>
2022-12-02 18:57:11 +0700

    Run systems in Update/FixedUpdate

--- Assets/Scripts/Infrastructure/Installers/ArenaInstaller.cs
+++ Assets/Scripts/Infrastructure/Installers/ArenaInstaller.cs
@@ -1,13 +1,17 @@
-﻿using Leopotam.Ecs;
+﻿using System;
+using Leopotam.Ecs;
 using UnityEngine;
 using Zenject;
 
 namespace Infrastructure.Installers
 {
-    public class ArenaInstaller : MonoInstaller, IInitializable, ITickable
+    public class ArenaInstaller : MonoInstaller, IInitializable
     {
         private EcsWorld _world;
         private EcsSystems _systems;
+
+        private bool _initializationCompleted = false;
+        private EcsSystems _simulationSystems;
 
         public override void InstallBindings()
         {
@@ -18,14 +22,25 @@ namespace Infrastructure.Installers
         {
             _world = new EcsWorld();
             _systems = new EcsSystems(_world);
+            _simulationSystems = new EcsSystems(_world);
 
             _systems.Add(new TestSystem());
 
             _systems.Init();
+            _initializationCompleted = true;
         }
 
-        public void Tick() =>
+        private void Update()
+        {
+            if (!_initializationCompleted) return;
             _systems.Run();
+        }
+
+        private void FixedUpdate()
+        {
+            if (!_initializationCompleted) return;
+            _simulationSystems.Run();
+        }
     }
 
     public struct TestSystem : IEcsInitSystem
