d221a868fac431c2286a27d7699e16031b207d33
Dmitry Frolov <frdima19@gmail.com>
2022-12-06 21:07:45 +0700

    Update ArenaInstaller: resolve systems and run

--- Assets/Scripts/Infrastructure/Installers/ArenaInstaller.cs
+++ Assets/Scripts/Infrastructure/Installers/ArenaInstaller.cs
@@ -1,32 +1,48 @@
 ﻿using System;
+using Extensions;
+using Game.Features.Character;
+using Game.Features.Character.Systems;
 using Leopotam.Ecs;
 using UnityEngine;
 using Zenject;
+using CharacterInfo = StaticData.CharacterInfo;
 
 namespace Infrastructure.Installers
 {
     public class ArenaInstaller : MonoInstaller, IInitializable
     {
+        [SerializeField] private CharacterInfo CharacterData;
+
         private EcsWorld _world;
         private EcsSystems _systems;
+        private EcsSystems _simulationSystems;
 
         private bool _initializationCompleted = false;
-        private EcsSystems _simulationSystems;
 
         public override void InstallBindings()
         {
+            Container.Bind<CharacterInfo>().FromInstance(CharacterData).AsCached();
             Container.Bind<IInitializable>().To<ArenaInstaller>().FromInstance(this);
+            Container.Bind<CharacterFactory>().ToSelf().AsSingle();
+            Container.Bind<SpawnCharacterSystem>().ToSelf().AsSingle();
+            Container.Bind<MoveCharacterSystem>().ToSelf().AsSingle();
+            Container.Bind<EcsWorld>().ToSelf().AsSingle();
         }
 
         public void Initialize()
         {
-            _world = new EcsWorld();
+            _world = Container.Resolve<EcsWorld>();
             _systems = new EcsSystems(_world);
             _simulationSystems = new EcsSystems(_world);
 
-            _systems.Add(new TestSystem());
+            _systems
+                .AddResolved<SpawnCharacterSystem>(Container);
+
+            _simulationSystems
+                .AddResolved<MoveCharacterSystem>(Container);
 
             _systems.Init();
+            _simulationSystems.Init();
             _initializationCompleted = true;
         }
 
