271c567a1d7a8ff36fb0fffc46ab44c0197ddb28
Dmitry Frolov <frdima19@gmail.com>
2022-12-06 18:40:29 +0700

    Update CharacterFactory: attach CharacterView

---
+++ Assets/Scripts/Game/Features/Character/Behaviours/CharacterView.cs
@@ -0,0 +1,12 @@
+﻿using Game.Services;
+using UnityEngine;
+
+namespace Game.Features.Character.Behaviours
+{
+    public class CharacterView : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+
+        [SerializeField] private EntityReference _entity;
+    }
+}
--- Assets/Scripts/Game/Features/Character/CharacterFactory.cs
+++ Assets/Scripts/Game/Features/Character/CharacterFactory.cs
@@ -1,29 +1,56 @@
-﻿using Game.Features.Health;
+﻿using Game.Features.Character.Behaviours;
+using Game.Features.Health;
 using Game.Features.Movement;
 using Game.Features.Tags;
+using Game.Services;
+using Game.Shared.Components;
 using Leopotam.Ecs;
 using UnityEngine;
+using CharacterInfo = StaticData.CharacterInfo;
 
 namespace Game.Features.Character
 {
     public class CharacterFactory
     {
         private readonly EcsWorld _world;
+        private readonly CharacterInfo _characterInfo;
 
-        public CharacterFactory(EcsWorld world)
+        public CharacterFactory(EcsWorld world, CharacterInfo characterInfo)
         {
+            _characterInfo = characterInfo;
             _world = world;
         }
 
-        public EcsEntity CreatePlayer(Vector3 position)
+        public EcsEntity CreatePlayerEntity(Vector3 position)
         {
+            var maxHealth = _characterInfo.BaseStats.MaxHealth;
+
             EcsEntity entity = _world.NewEntity();
             entity.Get<CharacterTag>();
-            entity.Replace(new PositionComponent {Position = position});
-            entity.Replace(new HealthComponent {MaxHealth = 100, Health = 100});
-            entity.Replace(new ExperienceComponent {Level = 1, CurrentXP = 0, ExperienceCapacity = 1000});
-            
+
+            entity
+                .Replace(new PositionComponent {Position = position})
+                .Replace(new HealthComponent {MaxHealth = maxHealth, Health = maxHealth})
+                .Replace(new ExperienceComponent {Level = 1, CurrentXP = 0, ExperienceCapacity = 1000});
+
+            SpawnCharacter(position, entity);
+
             return entity;
+        }
+
+        private CharacterView SpawnCharacter(Vector3 position, EcsEntity playerEntity)
+        {
+            var go = Object.Instantiate(_characterInfo.CharacterPrefab, position, Quaternion.identity);
+            var entityReference = go.GetComponent<EntityReference>();
+            var view = go.GetComponent<CharacterView>();
+
+            entityReference.Entity = playerEntity;
+
+            playerEntity
+                .Replace(new UnityObject<EntityReference> {Value = entityReference})
+                .Replace(new UnityObject<CharacterView> {Value = view});
+
+            return view;
         }
     }
 }
