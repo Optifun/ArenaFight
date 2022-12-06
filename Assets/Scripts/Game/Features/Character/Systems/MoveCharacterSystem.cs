c471547045e259c1be80bb20ea898ca002a5e3a0
Dmitry Frolov <frdima19@gmail.com>
2022-12-06 21:05:58 +0700

    Update MoveCharacterSystem: use lerp

--- Assets/Scripts/Game/Features/Character/Systems/MoveCharacterSystem.cs
+++ Assets/Scripts/Game/Features/Character/Systems/MoveCharacterSystem.cs
@@ -1,4 +1,5 @@
-﻿using Extensions;
+﻿using System;
+using Extensions;
 using Game.Features.Movement;
 using Game.Features.Tags;
 using Leopotam.Ecs;
@@ -9,19 +10,28 @@ namespace Game.Features.Character.Systems
     public class MoveCharacterSystem : IEcsRunSystem
     {
         private EcsFilter<InputComponent> _input;
-        private EcsFilter<CharacterTag, PositionComponent> _character;
+        private EcsFilter<CharacterTag, SpeedComponent> _character;
 
         public void Run()
         {
             foreach (var i in _character)
             {
-                ref var playerPosition = ref _character.Get2(i);
+                ref var playerSpeed = ref _character.Get2(i);
+
                 if (_input.TryGet1(out var input) && !input.IsNull())
                 {
                     ref var inputComponent = ref input.Unref();
-                    playerPosition.Position += inputComponent.Direction * Time.fixedTime;
+                    Vector3.LerpUnclamped(playerSpeed.Speed, inputComponent.Direction * playerSpeed.MaximumSpeed,
+                        SmoothStep(Time.fixedTime));
                 }
             }
+        }
+
+        private float SmoothStep(float t)
+        {
+            float v1 = t * t;
+            float v2 = 1 - (1 - t) * (1 - t);
+            return Mathf.Lerp(v1, v2, t);
         }
     }
 }
