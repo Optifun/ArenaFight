7285956d67ee7b770638562ce65af57b84d1c403
Dmitry Frolov <frdima19@gmail.com>
2022-12-06 18:20:08 +0700

    Create TriggerZone & TriggerEventComponent

---
+++ Assets/Scripts/Game/Services/EntityReference.cs
@@ -0,0 +1,10 @@
+﻿using Leopotam.Ecs;
+using UnityEngine;
+
+namespace Game.Services
+{
+    public class EntityReference : MonoBehaviour
+    {
+        public EcsEntity Entity { get; set; }
+    }
+}
---
+++ Assets/Scripts/Game/Services/TriggerZone.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Game.Features.Events;
+using Leopotam.Ecs;
+using UnityEngine;
+
+namespace Game.Services
+{
+    public class TriggerZone<TTag> : MonoBehaviour where TTag : struct
+    {
+        private EntityReference _entity;
+        private EcsWorld _world;
+
+        public void Init(EntityReference target, EcsWorld world)
+        {
+            _entity = target;
+            _world = world;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.TryGetComponent<EntityReference>(out var reference))
+                return;
+
+            TryRegisterEvent(reference, TriggerEventType.Enter);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (!other.TryGetComponent<EntityReference>(out var reference))
+                return;
+
+            TryRegisterEvent(reference, TriggerEventType.Stay);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!other.TryGetComponent<EntityReference>(out var reference))
+                return;
+
+            TryRegisterEvent(reference, TriggerEventType.Exit);
+        }
+
+        private void TryRegisterEvent(EntityReference reference, TriggerEventType eventType)
+        {
+            if (reference.Entity.Has<TTag>())
+                _world.NewEntity().Replace(new TriggerEventComponent
+                {
+                    Source = _entity.Entity,
+                    Target = reference.Entity,
+                    Type = eventType
+                });
+        }
+    }
+}
