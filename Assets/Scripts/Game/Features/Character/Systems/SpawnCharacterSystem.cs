8010f6e5b32bb9e67d95d2aecef3b08ce6df6ce8
Dmitry Frolov <frdima19@gmail.com>
2022-12-06 21:07:04 +0700

    Update SpawnCharacterSystem: use factory

--- Assets/Scripts/Game/Features/Character/Systems/SpawnCharacterSystem.cs
+++ Assets/Scripts/Game/Features/Character/Systems/SpawnCharacterSystem.cs
@@ -1,14 +1,20 @@
 ﻿using Leopotam.Ecs;
+using UnityEngine;
 
 namespace Game.Features.Character.Systems
 {
     public class SpawnCharacterSystem : IEcsInitSystem
     {
-        private CharacterFactory _factory;
-        
+        private readonly CharacterFactory _factory;
+
+        public SpawnCharacterSystem(CharacterFactory factory)
+        {
+            _factory = factory;
+        }
+
         public void Init()
         {
-            
+            _factory.CreatePlayerEntity(Vector3.zero);
         }
     }
 }
