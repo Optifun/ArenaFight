db6244fdc2338abdb45b6610a72ee4c4188e7bc0
Dmitry Frolov <frdima19@gmail.com>
2022-12-11 20:12:55 +0700

    Add Cleric animation: idle, walk, running

--- Assets/Scripts/Game/Features/Character/Behaviours/CharacterView.cs
+++ Assets/Scripts/Game/Features/Character/Behaviours/CharacterView.cs
@@ -10,6 +10,8 @@ namespace Game.Features.Character.Behaviours
         [SerializeField] private Animator _animator;
         [SerializeField] private Transform _model;
 
+        private static readonly int WalkSpeed = Animator.StringToHash("WalkSpeed");
+
 
         public override void OnViewStateUpdated()
         {
@@ -19,6 +21,8 @@ namespace Game.Features.Character.Behaviours
                 var nextRotation = Quaternion.LookRotation(speedComponent.Speed);
                 _model.rotation = Quaternion.Lerp(nextRotation, _model.rotation, 0.1f);
             }
+
+            _animator.SetFloat(WalkSpeed, speedComponent.Speed.sqrMagnitude);
         }
     }
 }
